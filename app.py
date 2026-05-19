832e0b0f5320d9e85b1687fc872fd8c1148a2376
alyastft <alyaastft@students.unnes.ac.id>
2026-05-19 20:55:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,10 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
-st.title("Sales Forecasting")
-
-uploaded_file = st.file_uploader(
-    "Upload Excel File",
-    type=['xlsx', 'csv']
+st.set_page_config(
+    page_title='Forecasting System',
+    layout='wide'
 )
 
-if uploaded_file is not None:
+st.title('Sales Forecasting System')
 
-    # READ FILE
-    if uploaded_file.name.endswith('.csv'):
-        df = pd.read_csv(uploaded_file)
-    else:
-        df = pd.read_excel(uploaded_file)
-
-    st.write(df.head())
-
-    # PILIH MODEL
-    selected_model = st.selectbox(
-        "Choose Model",
-        df['Model'].unique()
-    )
-
-    # FILTER
-    item_df = df[df['Model'] == selected_model]
-
-    # PLOT
-    fig, ax = plt.subplots(figsize=(10,5))
-
-    ax.plot(item_df['ds'], item_df['y'])
-
-    ax.set_title(selected_model)
-
-    st.pyplot(fig)
-
-    # CLASSIFICATION OTOMATIS
-    st.write(f"Category: {category}")
-    st.write(f"CV: {cv}")
-    st.write(f"Trend: {trend}")
-
-
+st.write('Upload dataset to start forecasting.')
