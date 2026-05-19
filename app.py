db6cddb98336fb6ab591de8be07525bbac36a4df
alyastft <alyaastft@students.unnes.ac.id>
2026-05-19 21:55:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,43 +2,34 @@ import streamlit as st
 import pandas as pd
 
 from utils.preprocessing import preprocess_data
-from utils.classification import classify_items
 
 st.set_page_config(
     page_title='Forecasting System',
     layout='wide'
 )
 
-st.title("Sales Forecasting System")
+st.title('Sales Forecasting System')
 
 uploaded_file = st.file_uploader(
-    "Upload Excel File",
-    type=["xlsx"]
+    'Upload Excel/CSV File',
+    type=['xlsx', 'csv']
 )
 
 if uploaded_file is not None:
 
-    try:
+    if uploaded_file.name.endswith('.xlsx'):
 
         df = pd.read_excel(uploaded_file)
 
-        st.subheader("Raw Dataset")
-        st.dataframe(df.head())
+    else:
 
-        df = preprocess_data(df)
+        df = pd.read_csv(uploaded_file)
 
-        st.subheader("Processed Dataset")
-        st.dataframe(df.head())
+    st.write('Raw Dataset')
+    st.dataframe(df.head())
 
-        classification_df = classify_items(df)
+    # preprocessing
+    df = preprocess_data(df)
 
-        st.subheader("Classification Result")
-        st.dataframe(classification_df)
-
-    except Exception as e:
-
-        st.error(f'ERROR: {e}')
-
-else:
-
-    st.info("Please upload Excel file")
+    st.write('Processed Dataset')
+    st.dataframe(df.head())
