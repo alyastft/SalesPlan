50b9933142e16aa7f523c5efb27d63d2db0bee47
alyastft <alyaastft@students.unnes.ac.id>
2026-05-19 20:56:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+
 
 st.set_page_config(
     page_title='Forecasting System',
@@ -8,3 +10,20 @@ st.set_page_config(
 st.title('Sales Forecasting System')
 
 st.write('Upload dataset to start forecasting.')
+
+uploaded_file = st.file_uploader(
+    'Upload Excel/CSV File',
+    type=['xlsx', 'csv']
+)
+
+if uploaded_file is not None:
+
+    if uploaded_file.name.endswith('.xlsx'):
+        df = pd.read_excel(uploaded_file)
+
+    else:
+        df = pd.read_csv(uploaded_file)
+
+    st.success('Dataset uploaded successfully')
+
+    st.dataframe(df.head())
