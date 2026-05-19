165c5154303b81ac8cd9b026cd8d1e6352167753
alyastft <alyaastft@students.unnes.ac.id>
2026-05-19 22:20:56 +0700

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,27 +1,49 @@
 import pandas as pd
 
+
 def preprocess_data(df):
 
     month_mapping = {
+        'Jan': 'Jan',
+        'Feb': 'Feb',
+        'Mar': 'Mar',
+        'Apr': 'Apr',
         'Mei': 'May',
+        'Jun': 'Jun',
+        'Jul': 'Jul',
         'Agu': 'Aug',
+        'Sep': 'Sep',
         'Okt': 'Oct',
+        'Nov': 'Nov',
         'Des': 'Dec'
     }
 
     df['Date'] = df['Date'].astype(str)
 
     for indo, eng in month_mapping.items():
-        df['Date'] = df['Date'].str.replace(indo, eng)
 
+        df['Date'] = df['Date'].str.replace(
+            indo,
+            eng
+        )
+
+    # convert format Jan-21
     df['Date'] = pd.to_datetime(
         df['Date'],
-        format='mixed'
+        format='%b-%y'
     )
 
+    # ubah jadi awal bulan
+    df['Date'] = df['Date'].dt.to_period('M')
+
+    df['Date'] = df['Date'].dt.to_timestamp()
+
     df = df.rename(columns={
+
         'Date': 'ds',
+
         'Sales': 'y'
+
     })
 
     return df
