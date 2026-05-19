63728076c73070d0d449932dc9b8dc63a2c92c7b
alyastft <alyaastft@students.unnes.ac.id>
2026-05-19 22:07:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from utils.preprocessing import preprocess_data
 from utils.classification import classify_items
+from utils.helper import RECOMMENDED_MODELS
 
 st.set_page_config(
     page_title='Forecasting System',
@@ -37,3 +38,26 @@ if uploaded_file is not None:
     classification_df = classify_items(df)
     st.write('Trend Classification')
     st.dataframe(classification_df)
+    
+    # Dropdown item
+    selected_model = st.selectbox(
+        'Choose Item',
+        classification_df['Model']
+    )
+    
+    # Take Category
+    selected_row = classification_df[
+        classification_df['Model'] == selected_model
+    ].iloc[0]
+    
+    category = selected_row['Category']
+    
+    # Insight
+    st.subheader('Trend Analysis')
+    
+    st.write(f'Category: {category}')
+    
+    st.write(f'''
+    Recommended Models:
+    {", ".join(RECOMMENDED_MODELS[category])}
+    ''')
