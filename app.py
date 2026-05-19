f1ed3c68f1d093cc4dbf763b38cc39e16b71e24d
alyastft <alyaastft@students.unnes.ac.id>
2026-05-19 21:43:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import pandas as pd
 
 from utils.preprocessing import preprocess_data
 from utils.classification import classify_items
-from utils.helper import RECOMMENDED_MODELS
 
 st.set_page_config(
     page_title='Forecasting System',
     layout='wide'
 )
 
-st.title("Sales Forecasting")
+st.title("Sales Forecasting System")
 
 uploaded_file = st.file_uploader(
     "Upload Excel File",
@@ -19,46 +18,27 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
 
-    df = pd.read_excel(uploaded_file)
+    try:
 
-    st.write("Raw Data")
-    st.dataframe(df.head())
+        df = pd.read_excel(uploaded_file)
 
-    df = preprocess_data(df)
+        st.subheader("Raw Dataset")
+        st.dataframe(df.head())
 
-    st.write("Processed Dataset")
-    st.dataframe(df.head())
+        df = preprocess_data(df)
 
-    classification_df = classify_items(df)
+        st.subheader("Processed Dataset")
+        st.dataframe(df.head())
 
-    st.write("Trend Classification")
-    st.dataframe(classification_df)
+        classification_df = classify_items(df)
 
-    selected_model = st.selectbox(
-        'Choose Item',
-        classification_df['Model']
-    )
+        st.subheader("Classification Result")
+        st.dataframe(classification_df)
 
-    selected_row = classification_df[
-        classification_df['Model'] == selected_model
-    ].iloc[0]
+    except Exception as e:
 
-    category = selected_row['Category']
-
-    st.subheader('Trend Analysis')
-
-    st.write(f'Category: {category}')
-
-    st.write(f'''
-    Recommended Models:
-    {", ".join(RECOMMENDED_MODELS[category])}
-    ''')
-
-    selected_method = st.selectbox(
-        'Choose Forecasting Method',
-        RECOMMENDED_MODELS[category]
-    )
+        st.error(f'ERROR: {e}')
 
 else:
 
-    st.info("Please upload file first")
+    st.info("Please upload Excel file")
