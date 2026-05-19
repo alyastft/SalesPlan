2b91805b5fd81c40dbe05e29f9dedf7c1f8b9dfa
alyastft <alyaastft@students.unnes.ac.id>
2026-05-19 20:57:34 +0700

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,27 @@
-import
+import pandas as pd
+
+def preprocess_data(df):
+
+    month_mapping = {
+        'Mei': 'May',
+        'Agu': 'Aug',
+        'Okt': 'Oct',
+        'Des': 'Dec'
+    }
+
+    df['Date'] = df['Date'].astype(str)
+
+    for indo, eng in month_mapping.items():
+        df['Date'] = df['Date'].str.replace(indo, eng)
+
+    df['Date'] = pd.to_datetime(
+        df['Date'],
+        format='mixed'
+    )
+
+    df = df.rename(columns={
+        'Date': 'ds',
+        'Sales': 'y'
+    })
+
+    return df
