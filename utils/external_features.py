6e93f8235973a7df58943d606d79d75fb7714c17
alyastft <alyaastft@students.unnes.ac.id>
2026-06-14 17:26:28 +0700

    Update external_features.py

--- utils/external_features.py
+++ utils/external_features.py
@@ -4,69 +4,67 @@ import pandas as pd
 def load_external_features():
 
     usd = pd.read_excel(
-        "assets/KursUSD.xlsx"
+        'assets/KursUSD.xlsx'
     )
 
     jpy = pd.read_excel(
-        "assets/KursJPY.xlsx"
+        'assets/KursJPY.xlsx'
     )
 
     motor = pd.read_excel(
-        "assets/ProduksiSepedaMotorSeluruh.xlsx"
+        'assets/ProduksiSepedaMotorSeluruh.xlsx'
     )
 
-    usd["Tanggal"] = pd.to_datetime(
-        usd["Tanggal"]
+    usd['Tanggal'] = pd.to_datetime(
+        usd['Tanggal']
     )
 
-    jpy["Tanggal"] = pd.to_datetime(
-        jpy["Tanggal"]
+    jpy['Tanggal'] = pd.to_datetime(
+        jpy['Tanggal']
     )
 
-    motor["Bulan"] = pd.to_datetime(
-        motor["Bulan"]
+    motor['Bulan'] = pd.to_datetime(
+        motor['Bulan']
     )
 
-    usd["ds"] = usd["Tanggal"].dt.to_period(
-        "M"
-    ).dt.to_timestamp()
+    usd['ds'] = (
+        usd['Tanggal']
+        .dt.to_period('M')
+        .dt.to_timestamp()
+    )
 
-    jpy["ds"] = jpy["Tanggal"].dt.to_period(
-        "M"
-    ).dt.to_timestamp()
+    jpy['ds'] = (
+        jpy['Tanggal']
+        .dt.to_period('M')
+        .dt.to_timestamp()
+    )
 
-    motor["ds"] = motor["Bulan"].dt.to_period(
-        "M"
-    ).dt.to_timestamp()
+    motor['ds'] = (
+        motor['Bulan']
+        .dt.to_period('M')
+        .dt.to_timestamp()
+    )
 
     usd = usd[
-        ["ds", "Kurs USD/IDR"]
+        ['ds', 'Kurs USD/IDR']
     ]
 
     jpy = jpy[
-        ["ds", "Kurs JPY/IDR"]
+        ['ds', 'Kurs JPY/IDR']
     ]
 
     motor = motor[
         [
-            "ds",
-            "Domestik",
-            "Ekspor"
+            'ds',
+            'Domestik',
+            'Ekspor'
         ]
     ]
 
     external_df = (
         usd
-        .merge(
-            jpy,
-            on="ds",
-            how="outer"
-        )
-        .merge(
-            motor,
-            on="ds",
-            how="outer"
-        )
+        .merge(jpy, on='ds')
+        .merge(motor, on='ds')
     )
 
     return external_df
